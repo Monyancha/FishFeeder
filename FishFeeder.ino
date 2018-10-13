32d8e98dbbcc779f0e3eadb1b19ac599dad925d3
liourej <julien.lioure@gmail.com>
2018-10-13 16:19:50 +0200

    WIP refactoring and readme

--- FishFeeder.ino
+++ FishFeeder.ino
@@ -19,17 +19,16 @@ float elapsedHours = 0.0;
 
 Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
 
-void setup()
+void InitPins()
 {
-  Serial.begin(115200);
-
   pinMode(CMD, INPUT_PULLUP);
-
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
   pinMode(QUANTITY, INPUT);
+}
 
-  // initialize timer1
+void InitTimer()
+{
   noInterrupts();           // disable all interrupts
   TCCR1A = 0;
   TCCR1B = 0;
@@ -40,14 +39,39 @@ void setup()
   TCCR1B |= (1 << CS12);    // 256 prescaler
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
   interrupts();             // enable all interrupts
+}
 
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_RED, HIGH);
+void InitStepMotor()
+{
   small_stepper.setSpeed(200);
-
   motorLoopsNb = analogRead(QUANTITY) / 50;
   Serial.println("Motor loops number when feeding: ");
   Serial.println(motorLoopsNb);
+}
+
+void TurnLedRed()
+{
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_RED, HIGH);
+}
+
+void TurnLedGreen()
+{
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_RED, LOW);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  InitPins();
+
+  InitTimer();
+
+  TurnLedRed();
+
+  InitStepMotor();
 
   feeded = false;
 
@@ -61,26 +85,40 @@ ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
   elapsedHours = elapsedHours + (1.0 / 3600.0);
 }
 
-void loop()
+void TurnLedToGreenIfFeeded()
+{
+  if (feeded) {
+    TurnLedGreen();
+  } else {
+    TurnLedRed();
+  }
+}
+
+void ManualFeed()
 {
   if (digitalRead(CMD) == false) {
     small_stepper.step(STEPS_PER_OUTPUT_REVOLUTION);
     Serial.println("User choose to manually feed");
   }
+}
+
+void AutomaticFeed()
+{
+  if (feeded == true)
+    return;
 
   // Feed (Arduino is powered at 8 am)
-  if (feeded == false) {
-    if ( (elapsedHours + SET_POWERING_HOUR) >= SET_FEED_HOUR) {
-      small_stepper.step(motorLoopsNb * STEPS_PER_OUTPUT_REVOLUTION);
-      feeded = true;
-    }
+  if ( (elapsedHours + SET_POWERING_HOUR) >= SET_FEED_HOUR) {
+    small_stepper.step(motorLoopsNb * STEPS_PER_OUTPUT_REVOLUTION);
+    feeded = true;
   }
+}
 
-  if (feeded) {
-    digitalWrite(LED_GREEN, HIGH);
-    digitalWrite(LED_RED, LOW);
-  } else {
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_RED, HIGH);
-  }
+void loop()
+{
+  ManualFeed();
+
+  AutomaticFeed();
+
+  TurnLedToGreenIfFeeded();
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+
+# <p align="center">Automatic fish feeder</p>
+
+<img src="/ReadmePictures/aquarium.jpg">
+
+
+<img src="/ReadmePictures/zoom.jpg">
